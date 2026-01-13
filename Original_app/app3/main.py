4a45e1ba0d4e6d423f9102f6174111198958f25c
Jiangt333 <jt1170504445@163.com>
2026-01-13 11:59:12 +0800

    feat: 模仿modified文件夹下的main，添加时间戳

--- Original_app/app3/main.py
+++ Original_app/app3/main.py
@@ -1,14 +1,18 @@
+import time
+init_st = time.time() * 1000
 import logging
 import numpy as np
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.info('Loading function')
-
+init_ed = time.time() * 1000
 def lambda_handler(event, context):
+    fun_st = time.time() * 1000
     print('/home/jiangt/FaaSLight-Re/Original_app/app3-dynamic/main.py=lambda_handler=12')
     lib_version = {'numpy': np.__version__}
     logger.info(lib_version)
-    return lib_version
+    fun_ed = time.time() * 1000
+    return ",InitStart:{},".format(init_st)+"InitEnd:{},".format(init_ed)+"functionStart:{},".format(fun_st)+"functionEnd:{},".format(fun_ed)
 
 # if __name__ == "__main__":
 #     lambda_handler(None, None)
--- Original_app/app3_original/main.py
+++ Original_app/app3_original/main.py
@@ -1,13 +1,17 @@
+import time
+init_st = time.time() * 1000
 import logging
 import numpy as np
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.info('Loading function')
-
+init_ed = time.time() * 1000
 def lambda_handler(event, context):
+    fun_st = time.time() * 1000
     lib_version = {'numpy': np.__version__}
     logger.info(lib_version)
-    return lib_version
+    fun_ed = time.time() * 1000
+    return ",InitStart:{},".format(init_st)+"InitEnd:{},".format(init_ed)+"functionStart:{},".format(fun_st)+"functionEnd:{},".format(fun_ed)
 
 # if __name__ == "__main__":
 #     lambda_handler(None, None)
